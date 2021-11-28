431851c423c767ffec87de6aa7fec6146232e9f0
Dmitriy Larionov <larionov.d.y@gmail.com>
2021-11-28 10:33:56 +0300

    Добавьте файлы проекта.

---
+++ HomeWork12/Circle.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork12
+{
+    public static class Circle
+    {
+        public const double PI = 3.1415926535897931;   
+
+        public static double Length(double radius)      //метод возвращает значение длины окружности
+        {
+            if (radius <= 0)
+            {
+                throw new Exception("Радиус не может быть нулевым или отрицательным значением");
+            }
+            else
+            {
+                double circleLength = 2 * PI * radius;
+                return circleLength;
+            }
+        }
+        public static double Area(double radius)      //метод возвращает значение площади круга
+        {
+            if (radius <= 0)
+            {
+                throw new Exception("Радиус не может быть нулевым или отрицательным значением");
+            }
+            else
+            {
+                double circleArea = Math.Pow(radius, 2) * PI;
+                return circleArea;
+            }
+        }
+        public static bool PointPosition(double radius, double centerCircleX0, double centerCircleY0, double pointX, double pointY)     //Метод определяет принадлежит ли точка с координатами x,y плоскостиб ограниченной окружностью с радиусом r и координатами центра x0, y0
+        {                                                                                                                               //в параметрах метода указывается:
+            bool value;                                                                                                                 //(радиус окружности, координата x центра окружности, координата y центра окружности, координата x проверяемой точки, координата y проверяемой точки) 
+            if (radius <= 0)                                                                                                            //метод возвращает булевое значение true, если принадлежит и соответственно false если нет
+            {
+                throw new Exception("Радиус не может быть нулевым или отрицательным значением");
+            }
+            else if (Math.Sqrt(Math.Pow(pointX - centerCircleX0, 2) + Math.Pow(pointY - centerCircleY0, 2)) <= radius)
+            {
+                value = true;
+            }
+            else
+            {
+                value = false;
+            }
+            return value;
+        }
+    }
+}
---
+++ HomeWork12/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork12
+{
+    //Разработать статический класс для работы с окружностью.Класс должен содержать 3 метода:
+    //метод, определяющий длину окружности по заданному радиусу;
+    //метод, определяющий площадь круга по заданному радиусу;
+    //метод, проверяющий принадлежность точки с координатами(x, y) кругу с радиусом r и координатами центра x0, y0.
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double radius = ReadValue("Введите пожалуйста значение радиуса:");
+            try
+            {
+                Console.WriteLine($"Длина окружности с радиусом {radius}, составляет: {Circle.Length(radius):f2}");
+                Console.WriteLine($"Площадь окружности с радиусом {radius}, составляет: {Circle.Area(radius):f2}");
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+            try
+            {
+                Console.WriteLine("Введите пожалуйста координаты центра окружности:");
+                double centerCircle_x0 = ReadValue("x0:");
+                double centerCircle_y0 = ReadValue("y0:");
+                Console.WriteLine("Введите пожалуйста координаты точки для проверки принадлежности к кругу:");
+                double point_x = ReadValue("x:");
+                double point_y = ReadValue("y:");
+                radius = ReadValue("Введите пожалуйста значение радиуса:");
+                Console.WriteLine($"Точка с указанными координатами X: {point_x} Y: {point_y} {(Circle.PointPosition(radius, centerCircle_x0, centerCircle_y0, point_x, point_y) == true ? "принадлежит" : "не принадлежит")} " +
+                    $"плоскости ограниченной окружностью с радиусом: {radius} и центром в точке X: {centerCircle_x0} Y: {centerCircle_y0}");
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+            Console.ReadKey();
+        }
+        static double ReadValue(string text)   //метод проверяющий корректность ввода данных
+        {
+            double value;
+            while (true)
+            {
+                Console.WriteLine(text);
+                if (Double.TryParse(Console.ReadLine(), out value))
+                {
+                    return value;
+                }
+                else
+                {
+                    Console.WriteLine("Ввод некорректен");
+                }
+            }
+        }
+    }
+}
+
+
+
+
